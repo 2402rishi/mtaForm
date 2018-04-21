7cde767dd1ef17696032ce5063d1f049875f1a7d
Rishi Agarwal <rishiagarwal@Rishis-MacBook-Pro.local>
2018-04-20 21:05:11 -0400

    Init5

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -13,8 +14,8 @@ app.config.update(dict(
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
-    MAIL_USERNAME = "",
-    MAIL_PASSWORD = "",
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD'),
 ))
 mail = Mail(app)
 from app import routes, models
